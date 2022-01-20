038fa4e9fe332193b51ebe1819fbe168774273eb
SuperOrteg <asseorteg@gmail.com>
2022-01-20 16:34:02 +0100

    All workin

--- script.js
+++ script.js
@@ -1,75 +1,96 @@
-const keywords = [];
+const buttons = [];
+
+const modal = document.getElementById('myModal');
+const title = document.querySelector('.modal-title');
+const content = document.querySelector('.modal-body');
+const image = document.querySelector('#modal-img');
 
 const showFeedPost = (movie) => {
-    document.querySelector('.articlesList').innerHTML += `
-        <article>
+    const placeholder = document.getElementsByClassName('articlesList')[0];
+  const feedPost = document.createElement("article")
+  feedPost.className = 'content-movie'
+  feedPost.innerHTML = `
             <h2>${movie.Title}</h2>
             <img src="${movie.Poster}">
             <p>${movie.Year}</p>
-            <button class="btn" value="${movie.imdbID}">Read More</button>
-        </article>
+            <button class="btn btn-secondary lzy_img" id="${movie.imdbID}">Read More</button>
     `;
+
+    placeholder.appendChild(feedPost);
+
+  let card = document.getElementById(movie.imdbID);
+  card.addEventListener("click", () => {
+    getDetails(movie.imdbID);
+  });
+};
+
+const getDetails = (id) => {
+    const url = `http://www.omdbapi.com/?i=${id}&apikey=${API}`;
+
+    fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          openModal(response);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+};
+
+const openModal = (movie) => {
+    const close = document.querySelector('#close');
+    close.addEventListener('click', () => { closeModal() });
+    title.innerHTML = `${movie.Title}`;
+    content.innerHTML = `${movie.Plot}`;
+    image.src = `${movie.Poster}`
+    modal.style.display = "block";
+};
+
+const closeModal = () => {
+    modal.style.display = "none";
 };
 
 const cleanedKeyword = (keyword) => {
-    const cleanedKeyword = keyword;
+  const cleanedKeyword = keyword;
 
-    return cleanedKeyword.replace(/[^a-zA-Z ]/g, "").toLowerCase();
+  return cleanedKeyword.replace(/[^a-zA-Z ]/g, "").toLowerCase();
 };
 
 const cleanAll = () => {
-	document.querySelector('.articlesList').innerHTML = '';
+  document.querySelector(".articlesList").innerHTML = "";
 };
 
 const search = (input) => {
+  const url = `http://www.omdbapi.com/?s=${input}&apikey=${API}`;
 
-	const url = `http://www.omdbapi.com/?s=${input}&apikey=${API}`;
-
-	fetch(url)
-	  .then((response) => { return response.json(); })
-	  .then((response) => { response.Search.forEach(movies => showFeedPost(movies)); })
-	  .catch((error) => { console.error(error); });
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      response.Search.forEach((movies) => showFeedPost(movies));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 const submit = () => {
-	return search(inputElement);
-}
-
-const addNewKeyword = (label, keyword) => {
-    resetInput();
-
-    if (keywords.includes(keyword)) {
-        console.warn("You already added this keyword. Nothing happens.");
-        return;
-    }
-
-    if (!label || !keyword) {
-        console.error("It seems you forgot to write the label or keyword in the addNewKeyword function");
-        return;
-    }
-
-    keywords.push(keyword);
-    currentKeywords.push(keyword);
-
-    document.querySelector('.keywordsList').innerHTML += `
-        <div>
-            <input id="${label}" value="${keyword}" type="checkbox" checked onchange="toggleKeyword(this.value)">
-            <label for="${label}">${label}</label>
-        </div>
-    `;
-
-    reloadArticles();
-    resetKeywordsUl();
+  return search(inputElement);
 };
 
-window.addEventListener('DOMContentLoaded', () => {
-    const inputElement = document.querySelector('input[type="text"]');
+window.addEventListener("DOMContentLoaded", () => {
+  const inputElement = document.querySelector('input[type="text"]');
 
-    document.querySelector('.addKeywordsForm').addEventListener('submit', (event) => {
-        event.preventDefault();
-        const keywordInputValue = inputElement.value;
-        search(cleanedKeyword(keywordInputValue));
-        cleanAll();
+  document
+    .querySelector(".addKeywordsForm")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const keywordInputValue = inputElement.value;
+      search(cleanedKeyword(keywordInputValue));
+      cleanAll();
     });
 });
 
